b0cab2e5a84ac87f6f95d6907957cfb2df25044b
mattwoods123 <64228417+mattwoods123@users.noreply.github.com>
2024-04-19 11:11:15 -0400

    commit man

--- assignments/assignment1/main.cpp
+++ assignments/assignment1/main.cpp
@@ -30,15 +30,15 @@ float deltaTime;
 ew::Camera camera;
 ew::CameraController cameraController;
 
-float recVerts[] = {
-	1.0f, -1.0f, 1.0f, 0.0f,
-	-1.0f, -1.0, 0.0f, 0.0f,
-	-1.0f, 1.0f, 0.0f, 1.0f,
-
-	1.0f, 1.0f, 1.0f, 1.0f,
-	1.0f, -1.0f, 1.0f, 0.0f,
-	-1.0f, 1.0f, 0.0f,  1.0f
-};
+//float recVerts[] = {
+//	1.0f, -1.0f, 1.0f, 0.0f,
+//	-1.0f, -1.0, 0.0f, 0.0f,
+//	-1.0f, 1.0f, 0.0f, 1.0f,
+//
+//	1.0f, 1.0f, 1.0f, 1.0f,
+//	1.0f, -1.0f, 1.0f, 0.0f,
+//	-1.0f, 1.0f, 0.0f,  1.0f
+//};
 
 struct Material {
 	float Ka = 1.0; 
@@ -71,51 +71,87 @@ int main() {
 	ew::Transform monkeyTransform;
 	
 
-	unsigned int rectVA, rectVB;
-	glGenVertexArrays(1, &rectVA);
-	glGenBuffers(1, &rectVB);
-	glBindVertexArray(rectVA);
-	glBindBuffer(GL_ARRAY_BUFFER, rectVB);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(recVerts), &recVerts, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+	//unsigned int rectVA, rectVB;
+	//glGenVertexArrays(1, &rectVA);
+	//glGenBuffers(1, &rectVB);
+	//glBindVertexArray(rectVA);
+	//glBindBuffer(GL_ARRAY_BUFFER, rectVB);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(recVerts), &recVerts, GL_STATIC_DRAW);
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	//glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
 
 
 
+
+
+
+
+	//unsigned int FBO;
+	//glGenFramebuffers(1, &FBO);
+	//glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+	//unsigned int frameBufferTexture;
+	//glGenTextures(0, &frameBufferTexture);
+	//glBindTexture(GL_TEXTURE_2D, frameBufferTexture);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	//glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, frameBufferTexture, 0);
+
+	//unsigned int RBO;
+	//glGenRenderbuffers(1, &RBO);
+	//glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+	//glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight);
+	//glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+	//auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	//slide 6
+	unsigned int fbo, colorBuffer;
+
+	glCreateFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	glGenTextures(1, &colorBuffer);
+	glBindTexture(GL_TEXTURE_2D, colorBuffer);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, screenWidth, screenHeight);
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, colorBuffer, 0);
+
+	//slides 9
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK); //Back face culling
 	glEnable(GL_DEPTH_TEST); //Depth testing
-
-
-
-	unsigned int FBO;
-	glGenFramebuffers(1, &FBO);
-	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
-
-	unsigned int frameBufferTexture;
-	glGenTextures(0, &frameBufferTexture);
-	glBindTexture(GL_TEXTURE_2D, frameBufferTexture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, frameBufferTexture, 0);
-
-	unsigned int RBO;
-	glGenRenderbuffers(1, &RBO);
-	glBindRenderbuffer(GL_RENDERBUFFER, RBO);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
-
-	auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 	
+	unsigned int depthBuffer;
+
+	glGenTextures(1, &depthBuffer);
+
+	glBindTexture(GL_TEXTURE_2D, depthBuffer);
+
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT16, screenWidth, screenHeight);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
+
+
+
+	//if (fboStatus != GL_FRAMEBUFFER_COMPLETE) {
+	//	std::cout << "Error : " << fboStatus << std::endl;
+	//}
+
+	GLenum fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 	if (fboStatus != GL_FRAMEBUFFER_COMPLETE) {
-		std::cout << "Error : " << fboStatus << std::endl;
+		printf("Framebuffer incomplete: %d", fboStatus);
 	}
+
+	unsigned int rbo;
+
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
 
 
 
@@ -127,15 +163,19 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
-		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 
 
 		//RENDER
 		glClearColor(0.6f,0.8f,0.92f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+		glViewport(0, 0, screenWidth, screenHeight);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 
-		//glEnable(GL_DEPTH_TEST);
+
 
 		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
 
@@ -164,15 +204,15 @@ int main() {
 		shader.use();
 		shader.setInt("_MainTex", 0);
 
-		framebuffer.use();
-		framebuffer.setInt("_MainTex", 0);
 
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		framebuffer.use();
-		glBindVertexArray(rectVA);
-		glDisable(GL_DEPTH_TEST);
-		glBindTexture(GL_TEXTURE_2D, frameBufferTexture);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		//framebuffer.use();
+		//glBindVertexArray(rectVA);
+		//glDisable(GL_DEPTH_TEST);
+		//glBindTexture(GL_TEXTURE_2D, frameBufferTexture);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
 		drawUI();
